5402144593c7d7cca5661770a6b0bebc7e97a1ab
VladimirIanchenkov <ianchenkov.v@gmail.com>
2022-11-22 22:02:40 +0700

    исправил карту

--- source/js/map.js
+++ source/js/map.js
@@ -1,14 +1,14 @@
-setTimeout(function() {
-	let elem = document.createElement('script');
+setTimeout(function () {
+  let elem = document.createElement('script');
   elem.type = 'text/javascript';
-	elem.src = 'https://api-maps.yandex.ru/2.1/?apikey=aa2e24c8-e05f-4356-8994-4b3c5d7c4590&load=package.standard&lang=ru_RU';
-  elem.onload = function() {
+  elem.src = 'https://api-maps.yandex.ru/2.1/?apikey=aa2e24c8-e05f-4356-8994-4b3c5d7c4590&load=package.standard&lang=ru_RU';
+  elem.onload = function () {
     ymaps.ready(getYaMap);
   };
-	document.querySelector('body').appendChild(elem);
-}, 2000);
+  document.querySelector('body').appendChild(elem);
+  }, 2000);
 
-function getYaMap() {
+function getYaMap () {
   let myMap;
   myMap = new ymaps.Map('map', {
       center: [59.93955, 30.32358],
